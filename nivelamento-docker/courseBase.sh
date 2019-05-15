08afc9326f27973726fa8b1867cca43a05476e8d
Rodrigo Rech <rrech@thoughtworks.com>
2019-05-15 20:01:37 -0300

    Showing progress msg

--- nivelamento-docker/courseBase.sh
+++ nivelamento-docker/courseBase.sh
@@ -1,8 +1,12 @@
 apt update -y && apt upgrade -y
 apt install openjdk-8-jdk -y
 
+echo "done" >> /root/java-install-finished
+
 docker pull hello-world
 
 docker pull openjdk:8u181-jdk
 docker pull mongo:latest
 docker pull openjdk:8u201-jre-alpine3.9 
+
+echo "done" >> /root/docker-images-finished

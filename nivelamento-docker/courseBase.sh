c1d405568b56b86a3fb0b02a526ad3161b99fd70
Rodrigo Rech <rrech@thoughtworks.com>
2019-05-15 17:53:42 -0300

    Adding course base script

---
+++ nivelamento-docker/courseBase.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+docker pull hello-world
+
+docker pull openjdk:8u181-jdk
+docker pull mongo
+docker pull openjdk:8u201-jre-alpine3.9 

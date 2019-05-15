47855e54af94f8a9f21509cb9bafc25fcd73b4c9
Rodrigo Rech <rrech@thoughtworks.com>
2019-05-15 20:01:48 -0300

    Showing progress msg

---
+++ nivelamento-docker/assets/show_progress.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+show_progress()
+{
+  echo -n "Installing Java 8"
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while true; do 
+    sudo grep -i "done" /root/java-install-finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Java 8 Installed"
+  echo -n "Pulling images"
+  while true; do 
+    sudo grep -i "done" /root/docker-images-finished &> /dev/null
+    if [[ "$?" -ne 0 ]]; then     
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+  echo "Docker images done"
+}
+
+show_progress

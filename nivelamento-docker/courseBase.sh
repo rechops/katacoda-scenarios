5e9e41b4d416ba45193c0b90498fc71fb645a3f2
Rodrigo Rech <rrech@thoughtworks.com>
2019-05-15 18:08:12 -0300

    Changing apt install for java8

--- nivelamento-docker/courseBase.sh
+++ nivelamento-docker/courseBase.sh
@@ -1,6 +1,5 @@
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
+apt update -y && apt upgrade -y
+sudo apt install openjdk-8-jdk -y
 
 docker pull hello-world
 
